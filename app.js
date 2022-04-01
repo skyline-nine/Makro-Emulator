0d1d5aee9c2db89f8be381dfdf3fe8090eacc83e
skyline-nine <83595233+skyline-nine@users.noreply.github.com>
2022-04-01 12:23:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var path = require('path');
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
 
 app.listen(process.env.PORT || 4000, function () {
     console.log('Node app is working!');
