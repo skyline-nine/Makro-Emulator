3bfdd637c55581ca87054eb7123240e7d035a38c
skyline-nine <83595233+skyline-nine@users.noreply.github.com>
2022-03-31 23:39:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join('index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(process.env.PORT || 4000, function () {
