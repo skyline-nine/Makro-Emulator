b9fc5c04315105ee1988f56fedec8bae83944733
skyline-nine <83595233+skyline-nine@users.noreply.github.com>
2022-04-01 08:44:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var http = require('http');
-http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/html'
-  });
-  res.write('<!doctype html>\n<html lang="en">\n' + 
-    '\n<meta charset="utf-8">\n<title>Test web page on node.js</title>\n' + 
-    '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' + 
-    '\n\n<h1>Euro 2012 teams</h1>\n' + 
-    '<div id="content"><p>The teams in Group D for Euro 2012 are:</p><ul><li>England</li><li>France</li><li>Sweden</li><li>Ukraine</li></ul></div>' + 
-    '\n\n');
-  res.end();
-}).listen(8888, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8888');
+const http = require('http');
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello, User!</h1> <h3> Welcome Home</h3>\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port number::${port}`);
+});
