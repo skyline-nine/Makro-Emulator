36417aac60e40d2440bf274777f474c6d982ac36
skyline-nine <null>
2022-04-04 13:49:34 +0000

    fixed stuff

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ app.get('/about', function (req, res) {
     res.sendFile(path.join(__dirname, 'about.html'));
 	console.log('about page loaded')
 });
-
+app.get('/style', function (req, res) {
+	res.sendFile(path.join(__dirname, 'style.css'));
+	
+});
 app.listen(process.env.PORT || 3000, function () {
     console.log('Node app is working!');
 });
