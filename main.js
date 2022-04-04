78c1b8e79e0f2560249b6b1e81113e0e512af496
skyline-nine <null>
2022-04-04 13:43:56 +0000

    Updated files

--- main.js
+++ main.js
@@ -2,13 +2,18 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static(__dirname + '/'));
+// Html links
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
+	console.log('index loaded')
 });
-app.get('/', function (req, res) {
+// Broken 
+app.get('/about', function (req, res) {
     res.sendFile(path.join(__dirname, 'about.html'));
+	console.log('about page loaded')
 });
 
-app.listen(process.env.PORT || 4000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Node app is working!');
 });
