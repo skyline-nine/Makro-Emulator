871b819d7a7879ec4d4d930fc49139f2d944fedd
skyline-nine <null>
2022-04-04 17:47:05 +0000

    fixed some stuff broke some stuff

--- main.js
+++ main.js
@@ -2,19 +2,17 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static('public'));
+
 // Html links
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'index.html'));
+	res.sendFile(path.join(__dirname,'index.html'));
 	console.log('index loaded');
 });
-// fixed
+// broke it again
 app.get('/about', function (req, res) {
-	res.sendFile(path.join(__dirname, 'about.html'));
+	res.sendFile(path.join(__dirname,'about.html'));
 	console.log('about page loaded');
-});
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'style.css'));
 });
 app.listen(process.env.PORT || 3000, function () {
 	console.log('Node app is working!');
