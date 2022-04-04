a144ac024ac0b9ef943adcb0e0ebd2bbd79d28d8
skyline-nine <null>
2022-04-04 15:08:41 +0000

    Refromated code (for neatness)

--- main.js
+++ main.js
@@ -5,18 +5,17 @@ var path = require('path');
 app.use(express.static(__dirname + '/'));
 // Html links
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-	console.log('index loaded')
+	res.sendFile(path.join(__dirname, 'index.html'));
+	console.log('index loaded');
 });
 // fixed
 app.get('/about', function (req, res) {
-    res.sendFile(path.join(__dirname, 'about.html'));
-	console.log('about page loaded')
+	res.sendFile(path.join(__dirname, 'about.html'));
+	console.log('about page loaded');
 });
 app.get('/style', function (req, res) {
 	res.sendFile(path.join(__dirname, 'style.css'));
-	
 });
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Node app is working!');
+	console.log('Node app is working!');
 });
