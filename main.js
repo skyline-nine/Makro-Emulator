48af84366354e43f5a694e04fe8058a09a103c48
skyline-nine <null>
2022-04-05 17:31:06 +0000

    0.0.2 update

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var fs = require('fs');
+const router = express.Router();
 
-app.use(express.static('public'));
+// For program to access file DO NOT DELETE
+app.use(express.static(__dirname + '/'));
 
 // Html links
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname,'index.html'));
-	console.log('index loaded');
+	console.log('main page loading');
+	res.sendFile(path.join(__dirname, 'index.html'));
+	console.log('main page loaded');
 });
-// broke it again
+
 app.get('/about', function (req, res) {
-	res.sendFile(path.join(__dirname,'about.html'));
+	console.log('about page loading');
+	res.sendFile(path.join(__dirname, 'about.html'));
 	console.log('about page loaded');
 });
+
 app.listen(process.env.PORT || 3000, function () {
 	console.log('Node app is working!');
 });
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname,'/404.html'))
+})
